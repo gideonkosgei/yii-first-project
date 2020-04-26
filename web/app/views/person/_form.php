d07caf254059e6d008c2a2109f6b96491e33030b
Kipkosgei <G.Kipkosgei@cgiar.org>
2020-04-26 15:50:34 +0300

    first commit

---
+++ web/app/views/person/_form.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Person */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="person-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'firstname')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'lastname')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'mobile')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'address')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'age')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ web/app/views/person/_search.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\PersonSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="person-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'firstname') ?>
+
+    <?= $form->field($model, 'lastname') ?>
+
+    <?= $form->field($model, 'mobile') ?>
+
+    <?= $form->field($model, 'address') ?>
+
+    <?php // echo $form->field($model, 'age') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-outline-secondary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
